8805a080f3291347f6f4043c8300463b359da364
Sho Ikeda <tenibaka333@gmail.com>
2024-09-21 14:20:47 +0900

    chore: APIの形式修正

--- src/resources/useUserResource.ts
+++ src/resources/useUserResource.ts
@@ -3,12 +3,8 @@ import axios from "axios";
 export function useUserResource() {
   return {
     async query() {
-      const url = 'https://nogame-f18ee607639d.herokuapp.com/api/user';
-      return axios.get(url, {
-        headers: {
-          "Content-Type": 'application/json'
-        }
-      });
+      const url = 'https://nogame-f18ee607639d.herokuapp.com/api/user.json';
+      return axios.get(url);
     },
   };
 }
