dd6fc023a840a476abdc9bd1cf2c855d4aabaef3
Sho Ikeda <tenibaka333@gmail.com>
2024-09-23 15:22:03 +0900

    chore: 閉じるボタン削除

--- src/views/components/UiPointInfoDialog.vue
+++ src/views/components/UiPointInfoDialog.vue
@@ -8,14 +8,6 @@
         ポイントは今後商品と交換が可能になります<br>
         たくさん貯めておくといいことがあるかも？？
       </p>
-      <div class="modal-action">
-        <form method="dialog">
-          <!-- if there is a button in form, it will close the modal -->
-          <button class="btn">
-            閉じる
-          </button>
-        </form>
-      </div>
     </div>
     <form method="dialog" class="modal-backdrop">
       <button>close</button>
