ba2109e2b2273d73dc03767d1037280a721e24d8
Sho Ikeda <tenibaka333@gmail.com>
2024-09-23 13:09:54 +0900

    feature: 結果画面の暫定UI実装

--- src/views/components/UiGameResult.vue
+++ src/views/components/UiGameResult.vue
@@ -6,10 +6,15 @@ defineProps<{
 </script>
 
 <template>
-  <div>
-    ゲーム終了画面
-    result: {{ result }}
-    <button class="btn btn-success" @click="goToHome">
+  <div class="flex flex-col prose">
+    <h1>
+      {{ result === 'won'
+        ? '勝利です！！'
+        : result === 'lose'
+          ? '敗北です。。。'
+          : '引き分けになりました' }}
+    </h1>
+    <button class="btn btn-primary" @click="goToHome">
       ホームに戻る
     </button>
   </div>
